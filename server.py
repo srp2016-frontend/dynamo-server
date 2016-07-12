97e2735b932af185af9ddb709e11452bb53be0f3
Ryan Goldstein <ryanisaacg@gmail.com>
2016-07-12 11:38:20 -0400

    Continuous build system is now cross-platform

--- server.py
+++ server.py
@@ -2,10 +2,20 @@ import os
 import http.server
 import socketserver
 import subprocess
+import threading
+import time
 os.chdir("dynamo-website")
 
+if not os.path.exists("node_modules"):
+    subprocess.run(["npm install"])
+
+#A function to build the TS files in a loop 
+def continuous_build():
+    while True:
+        subprocess.run(['node_modules/typescript/bin/tsc', 'src/main.ts', '--outFile', 'bin/script.js'])
+        time.sleep(0.05)
 #Build the files
-subprocess.Popen('while : ; do ./build.sh ; done', shell = True)
+threading.Thread(target = continuous_build).start()
 
 PORT = 8000
 
