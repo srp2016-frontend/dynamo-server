bb1816d321f799d4ef1c182352d5bfbfb3161fd5
Ryan Goldstein <ryanisaacg@gmail.com>
2016-07-27 09:49:40 -0400

    Capitalized athelete

--- generate.py
+++ generate.py
@@ -1,10 +1,10 @@
 import random
 import math
-people = ["Brian DeLeonardis","Jack Dates", "Anthony Fasano", "Anthony Hamill", "Brandon Guglielmo", "Chase Moran", "Daniel Collins", "Kevin DeStefano", "Matthew Kumar", "Ryan Goldstein", "Tina Lu"]
+items = ["Brian DeLeonardis","Jack Dates", "Anthony Fasano", "Anthony Hamill", "Brandon Guglielmo", "Chase Moran", "Daniel Collins", "Kevin DeStefano", "Matthew Kumar", "Ryan Goldstein", "Tina Lu"]
 with open('countries', 'r') as data:
     nations = data.read().split('\n')
 positions = []
-for i in range(len(people)):
+for i in range(len(items)):
     positions.append([])
     velocity = random.randrange(25, 45)
     angle = (random.random() - 0.5) / 10
@@ -17,13 +17,13 @@ for i in range(len(people)):
 string = "["
 for j in range(20):
     string += "\n\"["
-    for i in range(len(people)):
-        person = people[i]
+    for i in range(len(items)):
+        item = items[i]
         x,y = positions[i][j]
         country = random.choice(nations)
         while country.strip() == "":
             random.choice(nations)
-        string += "new Item(" + str(x) + ", " + str(y) + ", '" + people[i] + "', 18, '" + country + "', 'athlete'),"
+        string += "new Item(" + str(x) + ", " + str(y) + ", '" + items[i] + "', 18, '" + country + "', 'Athlete'),"
     string += "]\","
 string += "]"
 print(string)
