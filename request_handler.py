326ec2721847830be37cbabdf9dcbf1b108507b4
Ryan Goldstein <ryanisaacg@gmail.com>
2016-07-28 09:37:34 -0400

    Made sure to stringify data sent to the client

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,6 @@
 from http.server import SimpleHTTPRequestHandler
 from data_manager import DataManager
+import json
 manager = DataManager()
 
 class Handler(SimpleHTTPRequestHandler):
@@ -7,7 +8,7 @@ class Handler(SimpleHTTPRequestHandler):
         length = int(self.headers['content-length'])
         data_string = self.rfile.read(length)
         frame = int(data_string)
-        response = manager.get(frame)
+        response = json.dumps(manager.get(frame))
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.send_header('Content-length',str(len(response)))
