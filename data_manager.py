a82942a2a3b76d8be7489b43dc17984f40f2cd72
Ryan Goldstein <ryanisaacg@gmail.com>
2016-07-28 09:19:20 -0400

    Switched to database communication over locally generated data

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,14 @@
-#TODO: Talk to a SQL server
+import pymongo
 import json
+with open("location", "r") as data:
+    location = data.read()
 class DataManager(object):
     def __init__(self):
-        with open("test_data", "r") as test:
-            contents = test.read()
-        self.values = json.loads(contents)
+        connection = pymongo.MongoClient('10.11.12.73', 27017)
+        db = connection.dynamo
+        collection = db.test
+        self.values = collection.find_one()['times']
+        print(self.values)
     def get(self, frame):
         if frame < len(self.values):
             return self.values[frame]
