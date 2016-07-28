6c853a8b0aba3a62a286d1b502630b37365f5658
Ryan Goldstein <ryanisaacg@gmail.com>
2016-07-28 09:23:49 -0400

    Added defaults for important fields

--- data_manager.py
+++ data_manager.py
@@ -1,14 +1,19 @@
 import pymongo
-import json
-with open("location", "r") as data:
-    location = data.read()
+ip = '10.11.12.73'
+port = 27017
 class DataManager(object):
     def __init__(self):
-        connection = pymongo.MongoClient('10.11.12.73', 27017)
+        connection = pymongo.MongoClient(ip, port)
         db = connection.dynamo
         collection = db.test
         self.values = collection.find_one()['times']
-        print(self.values)
+        for frame in self.values:
+            for item in frame:
+                item.setdefault('name', 'John Doe')
+                item.setdefault('x', 0)
+                item.setdefault('y', 0)
+                item.setdefault('type', 'object')
+                item.setdefault('affiliation', 'unknown')
     def get(self, frame):
         if frame < len(self.values):
             return self.values[frame]
