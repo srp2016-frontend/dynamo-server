623c311f8551678028668d7b5b29b31fd047b9b2
Ryan Goldstein <ryanisaacg@gmail.com>
2016-08-03 10:53:56 -0400

    Added an x offset so the responders move across a plane

--- data/RFJoin.py
+++ data/RFJoin.py
@@ -4,6 +4,7 @@ in_files = sys.argv[1:-1]
 info = []
 start_time = 32636
 total_time = 0
+x_offset = 6 * (len(in_files) - 1) 
 for file in in_files:
     with open(file, 'r') as data:
         contents = data.read()
@@ -13,10 +14,11 @@ for file in in_files:
         if line.strip() == "":
             continue
         items = line.split(" ")
-        x = float(items[0])
+        x = float(items[0]) + x_offset
         y = float(items[1])
         time = int(items[2]) - start_time + total_time
         info.append([x, y, time])
     total_time = int(lines[-2].split(" ")[2]) - start_time + total_time
+    x_offset -= 6
 with open(sys.argv[-1], 'w') as out:
     out.write(json.dumps(info))
