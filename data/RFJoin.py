4cbb78525f12684ddfdd32d4c81986a1c14f8c64
Ryan Goldstein <ryanisaacg@gmail.com>
2016-08-03 10:47:24 -0400

    Added passive RFID data and a script to join them into one file to process

---
+++ data/RFJoin.py
@@ -0,0 +1,22 @@
+import sys
+import json
+in_files = sys.argv[1:-1]
+info = []
+start_time = 32636
+total_time = 0
+for file in in_files:
+    with open(file, 'r') as data:
+        contents = data.read()
+    lines = contents.split("\n")
+    start_time = int(lines[0].split(" ")[2])
+    for line in lines:
+        if line.strip() == "":
+            continue
+        items = line.split(" ")
+        x = float(items[0])
+        y = float(items[1])
+        time = int(items[2]) - start_time + total_time
+        info.append([x, y, time])
+    total_time = int(lines[-2].split(" ")[2]) - start_time + total_time
+with open(sys.argv[-1], 'w') as out:
+    out.write(json.dumps(info))
